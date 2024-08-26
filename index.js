b0cd95edd2ff7cd25adc19b3aff0e0e501d68e8b
CAROL <k.alba.t96@gmail.com>
2024-08-25 22:19:16 -0500

    Guardar proyecto

--- index.js
+++ index.js
@@ -21,6 +21,31 @@ function encriptar () {
     tituloMensaje.textContent = "Ningún mensaje encontrado";
     parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
     alert("Debes ingresar algún texto");
+} 
 }
 
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+    let tituloMensaje = document.getElementById("titulo-mensaje");
+    let parrafo = document.getElementById("parrafo");
+    let muñeco = document.getElementById("muñeco");
+    let textoCifrado = texto
+    
+    .replace(/enter/gi, "e")
+    .replace(/imes/gi, "i")
+    .replace(/ai/gi, "a")
+    .replace(/ober/gi, "o")
+    .replace(/ufat/gi, "u");  
+
+    if (texto.length != 0) {
+        document.getElementById("texto").value = textoCifrado;
+        tituloMensaje.textContent = "Texto desencriptado con éxito";
+        parrafo.textContent = "";
+        muñeco.src = "./imge/desencriptado.png"; 
+    } else { // El else ahora está asociado al if de la función desencriptar
+        muñeco.src = "./imge/muñeco.jpg";
+        tituloMensaje.textContent = "Ningún mensaje encontrado";
+        parrafo.textContent = "Ingresa el texto que deseas encriptar o desencriptar";
+        alert("Debes ingresar algún texto");
+    }
 }
